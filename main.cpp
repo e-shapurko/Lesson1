eb49e27c88b6b893a719ff511fa217d312d8c4e2
Evgeniy <e-shapurko@users.noreply.github.com>
2018-10-12 19:14:01 +0300

    Update main.cpp
    Add main strings.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include  "lib.h"
 #include <spdlog/spdlog.h>
+#include <stdio.h>
 
 int main ()
 {
+	printf("build %d", version());
+	printf("Hello, World!");
 	auto logger = spdlog::stdout_logger_mt("console");
 	logger->info("version {} was started", version());
 
