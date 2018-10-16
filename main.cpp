72743d85da3bab2a7a72e23682964dd6727f7411
e-shapurko <e-shapurko@users.noreply.github.com>
2018-10-16 18:23:01 +0300

    Update main.cpp
    "CopyPasta_Blin"

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main ()
 {
 
 	std::cout << "build " << version() << std::endl;
-	std::cout << "Hello, World! " << version() << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
